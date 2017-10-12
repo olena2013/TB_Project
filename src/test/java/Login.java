687a689fd57641f859b37b16892695d94f4dbca3
olena2013 <ltunik@mail.ru>
2017-10-11 20:03:19 -0700

    Changes 2

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,36 +1,23 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+public class Login extends BaseClass {
 
-public class Login {
-    private WebDriver driver;
 
-    @BeforeTest
-    public void start(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lena\\IdeaProjects\\TB_Portal\\driver_chrome\\chromedriver.exe");
-        driver=new ChromeDriver();
-        driver.get("http://nrcmiscmoddev01:8034");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    @Test(description = "Test case perform invalid login")
+    public void loginToAppl(){
+        driver.findElement(By.id("login-email")).sendKeys("alena@consultnovo.com");
+        driver.findElement(By.id("login-password")).sendKeys("Portal21");
+        driver.findElement(By.cssSelector(".auth-box__main-action")).click();
+        Reporter.log("browser session end",true);
     }
 
-    @Test
-    public void login(){
+
+    @Test(description = "Test case perform valid login")
+    public void loginToApp(){
         driver.findElement(By.id("login-email")).sendKeys("olena@consultnovo.com");
         driver.findElement(By.id("login-password")).sendKeys("Portal@1");
         driver.findElement(By.cssSelector(".auth-box__main-action")).click();
-
-
-    }
-
-    @AfterTest
-    public void stop() {
-        driver.quit();
     }
 }
-
